5d2426aedc5c4062561f04ad9d0f92d5063813a6
Johntizzy <adepojujohn566@gmail.com>
2024-12-12 14:42:40 +0100

    complete website design

--- main.js
+++ main.js
@@ -42,15 +42,84 @@ const newArrival = [
 ];
 
 const newArrivalDiv = document.getElementById("newArrivalDiv");
+let count = 0;
+const wishlistCount = document.getElementById("wishlist-count");
 
-const productDiv = newArrival.map((product) => {
-   return `
+const productDiv = newArrival
+  .map((product) => {
+    return `
     <div class="new-arrival-box" >
             <img src="${product.image}" alt="${product.image}">
-            <span class="d-block text-secondary">${product.name}</span>
+            <div>
+             <span class="d-block text-secondary">${product.name}</span>
+            <ion-icon size="large" name="heart-outline" onclick="handleHeartClick(this)"></ion-icon>
+
+            </div>
             <span class="d-block text-secondary b">&#8358;${product.price}</span>
     </div>
-    `
-    
-}).join('');
-newArrivalDiv.innerHTML = productDiv
+    `;
+  })
+  .join("");
+newArrivalDiv.innerHTML = productDiv;
+
+// WISHLIST ICONS FUNCTION
+function handleHeartClick(icon) {
+  // icon.getAttribute('name')=="heart" && icon.setAttribute('name','heart-outline') ||
+  // icon.getAttribute('name')=="heart-outline" && icon.setAttribute('name','heart')
+
+  if (icon.getAttribute("name") == "heart") {
+    count -= 1;
+    icon.setAttribute("name", "heart-outline");
+  } else if (icon.getAttribute("name") == "heart-outline") {
+    icon.setAttribute("name", "heart");
+    count += 1;
+    console.log(icon.parentElement);
+  }
+  wishlistCount.innerHTML = count;
+}
+
+// YOU MAY ALSO LIKE
+
+const ymal = [
+  {
+    name: "STYLISH TOP",
+    price: 15000,
+    image: "styles/Assets/stylish-top.jpg",
+  },
+  {
+    name: "SAVAGE CAP",
+    price: 5000,
+    image: "styles/Assets/savage-cap.jpg",
+  },
+  {
+    name: "SAVAGE TOP",
+    price: 5000,
+    image: "styles/Assets/savage-top.jpg",
+  },
+  {
+    name: "SAVAGE HOODIE",
+    price: 5000,
+    image: "styles/Assets/savage-hoodie.jpg",
+  },
+];
+
+const ymalDiv = document.getElementById("ymalDiv");
+
+const ymalItems = ymal
+  .map((item) => {
+    return `
+                <div id="ymalBoxDiv">
+            <img src="${item.image}" alt="${item.name}" class="pb-2"/>
+            <div>
+              <span >${item.name}</span>
+              <ion-icon size="large" name="heart-outline" onclick="handleHeartClick(this)"></ion-icon>
+            </div>
+            <span class="pb-">&#8358;${item.price}</span>
+          </div>
+
+
+  `;
+  })
+  .join("");
+
+ymalDiv.innerHTML = ymalItems
